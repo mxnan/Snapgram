bf32897d6f636f06f5d1df5588a6c632625f20ec
mxnan <128995981+mxnan@users.noreply.github.com>
2023-11-07 14:10:57 +0530

    public added , created routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+
+import { Routes, Route } from 'react-router-dom'
 import './globals.css'
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+import { Home } from './_root/pages'
 
 const App = () => {
   return (
-    <div className="text-3xl bold underline">
-      App
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={ <AuthLayout />}>
+          <Route path="/sign-in" element={ <SigninForm />} />
+          <Route path="/sign-up" element={ <SignupForm />} />
+        </Route>
+        {/* private routes */}
+        <Route element={ <RootLayout />}>
+          <Route index element={< Home />} />
+        </Route>
+        
+      </Routes>
+    </main>
   )
 }
 
