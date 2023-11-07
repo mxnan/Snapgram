cca2f07642e2f6906e2e1b0c8cebef2d12979703
mxnan <128995981+mxnan@users.noreply.github.com>
2023-11-07 19:23:30 +0530

    creating formfields, reupdated css due to shadcn

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,10 +1,73 @@
+
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-
-
-
+import { useForm } from "react-hook-form"
+import { SignupValidation } from "@/lib/validation"
+import { z } from "zod"
+ 
 const SignupForm = () => {
+
+   // 1. Define your form.
+   const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <Button>Click me</Button>
+  
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="logo" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create A New Account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use Snapgram , enter your account details
+        </p>
+      
+      <form onSubmit={form.handleSubmit(onSubmit)} 
+       className="flex flex-col gap-5 w-full mt-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input"
+                placeholder="Your Name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      </div>
+    </Form>
+ 
   )
 }
 
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import * as z from "zod"
+
+
+export const SignupValidation = z.object({
+  name: z.string().min(2,{message: 'Too Short'}).max(30,{message:'Too Long'}),
+  username: z.string().min(2,{message: 'Too Short'}).max(20,{message:'Too Long'}),
+  email: z.string().email(),
+  password: z.string().min(8,{message:'Password must be atleast 8 characters'}),
+})
