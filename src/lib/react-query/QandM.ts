44ed41a43b6671ae14c302dee7736e07776e1d38
mxnan <128995981+mxnan@users.noreply.github.com>
2023-11-16 13:49:06 +0530

    signout function added

--- src/lib/react-query/QandM.ts
+++ src/lib/react-query/QandM.ts
@@ -4,7 +4,7 @@ import {
    useQueryClient,
    useInfiniteQuery,
   } from '@tanstack/react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
 
@@ -22,5 +22,11 @@ export const useSignInAccount = () => {
       password:string
         }) => 
     signInAccount(user)
+  })
+}
+
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount
   })
 }
